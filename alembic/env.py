d8152c711d6abc02b6fe1078217be39a733d1a1b
Neeraj Kashyap <nkashy1@gmail.com>
2021-08-27 09:46:09 -0700

    JournalTtl -> JournalTTL in alembic/env.py

--- alembic/env.py
+++ alembic/env.py
@@ -44,7 +44,7 @@ from spire.journal.models import (
     JournalEntry,
     JournalPermissions,
     SpireOAuthScopes,
-    JournalTtl,
+    JournalTTL,
 )
 from spire.preferences.models import DefaultJournal
 from spire.slack.models import (
@@ -80,7 +80,7 @@ def include_symbol(tablename, schema):
         JournalEntryTag.__tablename__,
         JournalPermissions.__tablename__,
         SpireOAuthScopes.__tablename__,
-        JournalTtl.__tablename__,
+        JournalTTL.__tablename__,
         GitHubOAuthEvent.__tablename__,
         GitHubBugoutUser.__tablename__,
         GitHubRepo.__tablename__,
