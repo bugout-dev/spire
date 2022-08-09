09b39988325f7f8dfe14a90f340e863dae937037
kompotkot <zenitsws@gmail.com>
2022-08-09 09:57:36 +0000

    Scm nix with correct rev commit hash

---
+++ scm/scm.nix
@@ -0,0 +1,28 @@
+with builtins;
+let
+    scm_repos = [
+        (getEnv "SCM_GIT")
+        (fetchGit {
+            url = "git@gitlab.com:deltaex/schematic.git";
+            rev = "ba5d7b40255e5da9a74e666dd88e309dae40fbd2";
+        })
+    ];
+    scm_repo = head (filter (x: x != "") scm_repos);
+    scm = (import scm_repo {
+        verbose = true;
+        repos = [
+            "."
+            (getEnv "MDP_GIT")
+            (fetchGit {
+                url = "git@github.com:bugout-dev/spire.git";
+                rev = "69b1af2b5c0d8c495fcadb85edf1ea39843b2286";
+            })
+        ] ++ scm_repos;
+    });
+in rec {
+    schematic = scm.shell.overrideAttrs ( oldAttrs : {
+        shellHook = oldAttrs.shellHook + ''
+            [ -n "$ENV" -a "$ENV" != "dev" ] && export BUGSNAG=2b987ca13cd93a4931bb746aace204fb
+        '';
+    });
+}
